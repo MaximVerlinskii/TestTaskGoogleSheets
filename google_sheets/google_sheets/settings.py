dffb6174d9600e0c13793bdd35f8a04f134fa0e9
Maxim Verlinskii <m.verlinsky@yandex.ru>
2022-06-23 10:29:36 +0700

    Database

--- google_sheets/google_sheets/settings.py
+++ google_sheets/google_sheets/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sheet_to_db'
 ]
 
 MIDDLEWARE = [
@@ -74,6 +75,11 @@ WSGI_APPLICATION = 'google_sheets.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
+
+
+
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
--- google_sheets/sheet_to_db/models.py
+++ google_sheets/sheet_to_db/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Orders(models.Model):
+
+    id_from_sheet = models.IntegerField(verbose_name='№')
+    number_of_order = models.IntegerField(verbose_name='заказ №')
+    price_usd = models.FloatField(verbose_name='стоимость,$')
+    delivery_time = models.DateTimeField(verbose_name='срок поставки')
+    price_rub = models.FloatField(verbose_name='стоимость в руб.')
+
